b1b52ebde38f357e1f41619079d4f7d980176589
VydaSriram <91535528+VydaSriram@users.noreply.github.com>
2021-10-07 21:25:27 +0530

    Add files via upload

---
+++ folder2/img.js
@@ -0,0 +1,37 @@
+
+function newImage1(a)
+{
+   document.getElementById("01").src="rohit.jpg";
+
+
+}
+
+function sameImage1(b)
+{
+   document.getElementById("01").src="dhoni.jpg";
+
+}
+function newImage2(a)
+{
+   document.getElementById("02").src="kohli.jpg";
+
+
+}
+
+function sameImage2(b)
+{
+   document.getElementById("02").src="iyer.jpg";
+
+}
+function newImage3(a)
+{
+   document.getElementById("03").src="pandya.jpg";
+
+
+}
+
+function sameImage3(b)
+{
+   document.getElementById("03").src="pant.jpg";
+
+}
